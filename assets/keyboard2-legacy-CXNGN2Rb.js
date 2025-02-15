e09e1a11472040504d952b363c9a34f84fca99f8
aryeeeeeeeeel <arielsumantin69@gmail.com>
2025-02-15 17:24:13 +0800

    Updates

--- assets/keyboard2-legacy-CXNGN2Rb.js
+++ assets/keyboard2-legacy-CXNGN2Rb.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CdrYx6oB.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-3kxZOk2P.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
