64715e29d664fc43c16f8b576cfa4a74caca49a1
aryeeeeeeeeel <arielsumantin69@gmail.com>
2025-02-17 13:40:30 +0800

    Updates

--- assets/keyboard2-legacy-Cl6NUEVw.js
+++ assets/keyboard2-legacy-Cl6NUEVw.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-3kxZOk2P.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-BFrfD-3h.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
