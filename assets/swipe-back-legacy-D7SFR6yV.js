91bda99424d8739b9aedc7e321e4f2e3ed55073b
aryeeeeeeeeel <arielsumantin69@gmail.com>
2025-03-14 01:25:19 +0800

    Updates

--- assets/swipe-back-legacy-D7SFR6yV.js
+++ assets/swipe-back-legacy-D7SFR6yV.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Bw8Lx9T7.js","./index3-legacy-Bu2hn5DS.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-B7lW2dD4.js","./index3-legacy-wBuVXk4D.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
