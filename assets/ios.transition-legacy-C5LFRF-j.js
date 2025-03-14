d33c8d151e60470ad9261b8d99a56bd6513f413d
aryeeeeeeeeel <arielsumantin69@gmail.com>
2025-03-15 02:06:21 +0800

    Updates

--- assets/ios.transition-legacy-C5LFRF-j.js
+++ assets/ios.transition-legacy-C5LFRF-j.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DCuPlJ-Y.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-BTzWV64_.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/keyboard2-legacy-DZ7oWwry.js
+++ assets/keyboard2-legacy-DZ7oWwry.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DCuPlJ-Y.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-BTzWV64_.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
