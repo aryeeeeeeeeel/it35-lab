f22440fd873d78e2d9713cd3b77a69db21a76f9f
aryeeeeeeeeel <arielsumantin69@gmail.com>
2025-03-26 22:00:05 +0800

    Updates

--- assets/ios.transition-legacy-B_sUDPft.js
+++ assets/ios.transition-legacy-B_sUDPft.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BTzWV64_.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-BHNpLM0A.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/keyboard2-legacy-CXjHyzHc.js
+++ assets/keyboard2-legacy-CXjHyzHc.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BTzWV64_.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-BHNpLM0A.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
