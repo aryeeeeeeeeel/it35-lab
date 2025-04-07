25e7627c3cf4d29c5a6aba7758e32ea5bf137b95
aryeeeeeeeeel <arielsumantin69@gmail.com>
2025-04-07 21:59:52 +0800

    Updates

--- assets/md.transition-legacy-CAbgFhMg.js
+++ assets/md.transition-legacy-CAbgFhMg.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Dwr5jxjg.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-CTEfxjHc.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
