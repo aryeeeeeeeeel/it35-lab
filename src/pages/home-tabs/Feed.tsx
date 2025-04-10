fc2c0436662137cf2242d7e780ee251faa11c18f
aryeeeeeeeeel <arielsumantin69@gmail.com>
2025-04-10 15:40:17 +0800

    Reverted changes on Feed.tsx

--- src/pages/home-tabs/Feed.tsx
+++ src/pages/home-tabs/Feed.tsx
@@ -21,50 +21,20 @@ import {
             <IonButtons slot='start'>
               <IonMenuButton></IonMenuButton>
             </IonButtons>
-            <IonTitle>Classroom</IonTitle>
+            <IonTitle>Feed</IonTitle>
           </IonToolbar>
         </IonHeader>
         <IonContent fullscreen>
-          <IonCard color="light">
-            <IonCardHeader>
-              <IonCardTitle>IT35B Application Development & Emerging Technologies</IonCardTitle>
-              <IonCardSubtitle>IT71 2nd Semester A.Y. 2024-2025</IonCardSubtitle>
-            </IonCardHeader>
-            <IonCardContent>NBSC Computer Studies</IonCardContent>
-            <IonButton fill="clear">Open</IonButton>
-          </IonCard>
-          <IonCard color="light">
-            <IonCardHeader>
-              <IonCardTitle>IT36B Information Assurance & Security 2</IonCardTitle>
-              <IonCardSubtitle>IT72 2nd Semester A.Y. 2024-2025</IonCardSubtitle>
-            </IonCardHeader>
-            <IonCardContent>NBSC Computer Studies</IonCardContent>
-            <IonButton fill="clear">Open</IonButton>
-          </IonCard>
-          <IonCard color="light">
-            <IonCardHeader>
-              <IonCardTitle>IT37B Elective 4 (Hardware Implemention Technologies)</IonCardTitle>
-              <IonCardSubtitle>IT73 2nd Semester A.Y. 2024-2025</IonCardSubtitle>
-            </IonCardHeader>
-            <IonCardContent>NBSC Computer Studies</IonCardContent>
-            <IonButton fill="clear">Open</IonButton>
-          </IonCard>
-          <IonCard color="light">
-            <IonCardHeader>
-              <IonCardTitle>IT38B Enterprise System</IonCardTitle>
-              <IonCardSubtitle>IT74 2nd Semester A.Y. 2024-2025</IonCardSubtitle>
-            </IonCardHeader>
-            <IonCardContent>NBSC Computer Studies</IonCardContent>
-            <IonButton fill="clear">Open</IonButton>
-          </IonCard>
-          <IonCard color="light">
-            <IonCardHeader>
-              <IonCardTitle>IT39B System Integration & Architecture 2</IonCardTitle>
-              <IonCardSubtitle>IT75 2nd Semester A.Y. 2024-2025</IonCardSubtitle>
-            </IonCardHeader>
-            <IonCardContent>NBSC Computer Studies</IonCardContent>
-            <IonButton fill="clear">Open</IonButton>
-          </IonCard>
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '100%',
+          }}
+        >
+          Feed
+        </div>
         </IonContent>
       </IonPage>
     );
