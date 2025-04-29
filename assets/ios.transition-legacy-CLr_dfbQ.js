ff7986c0b0dfa7d006fa255b25f400222f7e5547
aryeeeeeeeeel <arielsumantin69@gmail.com>
2025-04-29 22:44:20 +0800

    Updates

--- assets/ios.transition-legacy-CLr_dfbQ.js
+++ assets/ios.transition-legacy-CLr_dfbQ.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-ElPpKQ-w.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-CIKUKZdz.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/md.transition-legacy-BJzvPSdG.js
+++ assets/md.transition-legacy-BJzvPSdG.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-ElPpKQ-w.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-CIKUKZdz.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
